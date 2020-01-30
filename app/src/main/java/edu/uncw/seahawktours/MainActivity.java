0ed0e73470429cae8521406e4af34f1821dfdb0d
abolsoy <abolsoy@gmail.com>
2020-01-29 19:01:40 -0500

    Practice - Author

--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    // @ author Alex Bolsoy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
