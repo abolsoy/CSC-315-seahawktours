36231325d9f79ce984512f01ac1b7f577bb4a038
abolsoy <abolsoy@gmail.com>
2020-01-29 20:19:11 -0500

    Get layout .xml files working. Still need to get the Intent working.

---
+++ app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
@@ -0,0 +1,18 @@
+package edu.uncw.seahawktours;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        //Intent intent = getIntent();
+        //String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        //TextView messageView = (TextView)findViewById(R.id.message);
+        //messageView.setText(messageText);
+    }
+}
--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -2,13 +2,42 @@ package edu.uncw.seahawktours;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+
+    private String state;
+    private String building;
 
     // @ author Alex Bolsoy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            state = savedInstanceState.getString("state");
+            building = savedInstanceState.getString("building");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+    }
+
+    public void onClickSelect(View view) {
+        //Get a reference to the Spinner
+        Spinner building = (Spinner) findViewById(R.id.building);
+        //Get the selected item in the Spinner
+        String buildingName = String.valueOf(building.getSelectedItem());
+
+
+        //Intent intent = new Intent(Intent.ACTION_SEND);
+        //intent.setType("text/plain");
+        //intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        //String chooserTitle = getString(R.string.chooser);
+        //Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        //startActivity(chosenIntent);
     }
 }
