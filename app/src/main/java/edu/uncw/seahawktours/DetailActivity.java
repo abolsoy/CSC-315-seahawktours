f27c812fc7890dd36c5a26bd5ace0b945182e180
abolsoy <abolsoy@gmail.com>
2020-01-30 20:04:04 -0500

    Got the app mostly working. Still have to remember the last building that the user clicked on.

--- app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
@@ -1,18 +1,27 @@
 package edu.uncw.seahawktours;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends Activity { //AppCompatActivity caused problems!!!
+
+    public static final String EXTRA_BUILDING = "building";
+    public static final String EXTRA_INFO = "info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        //Intent intent = getIntent();
-        //String messageText = intent.getStringExtra(EXTRA_MESSAGE);
-        //TextView messageView = (TextView)findViewById(R.id.message);
-        //messageView.setText(messageText);
+        Intent intent = getIntent();
+        String buildingText = intent.getStringExtra(EXTRA_BUILDING);
+        String infoText = intent.getStringExtra(EXTRA_INFO);
+        TextView buildingView = findViewById(R.id.building_name);
+        TextView infoView = findViewById(R.id.building_info);
+        buildingView.setText(buildingText);
+        infoView.setText(infoText);
     }
 }
